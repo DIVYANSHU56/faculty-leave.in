a09968cbda65503c1b477c1efba39c02d3be13cf
Divyanshu Sharma <96658657+DIVYANSHU56@users.noreply.github.com>
2024-07-29 17:45:02 +0530

    add commit
    add

---
+++ myproject/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Employee Leave Management System in PHP | Developed By <a href="https://divyanshu56.github.io/portfolio.github.io/"> DIVYANSHU SHARMA</p>
+        </div>
+</footer>
